c3b99936b9e3f84766a0522b42725c74301bad7a
YangZhihao <2971387095@qq.com>
2022-03-21 01:09:32 +0800

    Week 4 Commit

--- src/main/java/com/YangZhihao/week3/demo/LifeCycleServlet.java
+++ src/main/java/com/YangZhihao/week3/demo/LifeCycleServlet.java
@@ -1,18 +1,39 @@
 package com.YangZhihao.week3.demo;
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+@WebServlet("/life")
 public class LifeCycleServlet extends HttpServlet {
+    Connection con=null;
     public LifeCycleServlet() {
         System.out.println("i am constructor-->LifeCycleServlet()");
     }
 
     @Override
     public void init() {
-        System.out.println("i am init");
+        ServletContext context=getServletContext();
+        String driver=context.getInitParameter("driver");
+        String url=context.getInitParameter("url");
+        String username=context.getInitParameter("username");
+        String password=context.getInitParameter("password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con= DriverManager.getConnection(url,username,password);
+            System.out.println("Connection -->"+con);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("i am init()->LifeCycleServlet-->"+con);
+
     }
 
     @Override
@@ -26,5 +47,10 @@ public class LifeCycleServlet extends HttpServlet {
     }
     public void destory(){
         System.out.println("i am in destory()");
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
