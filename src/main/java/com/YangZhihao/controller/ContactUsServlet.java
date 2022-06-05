f281db32cf68d955d78a26ee21a3644b249e219f
YangZhihao <2971387095@qq.com>
2022-06-05 19:46:08 +0800

    week15 Commit

---
+++ src/main/java/com/YangZhihao/controller/ContactUsServlet.java
@@ -0,0 +1,19 @@
+package com.YangZhihao.controller;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+@WebServlet(name = "ContactUsServlet", value = "/contactUs")
+public class ContactUsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path="/WEB-INF/views/contactUs.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
